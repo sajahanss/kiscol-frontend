ce8fc327e834ffeb7e33c96edd39b47924195da3
sajahanss <71567532+sajahanss@users.noreply.github.com>
2024-09-27 01:39:43 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -182,8 +182,8 @@ function Nav() {
           {navigation.map((item) => (
             <DisclosureButton
               key={item.name}
-              as="a"
-              href={item.href}
+              as="Link"
+              href={item.to}
               aria-current={item.current ? 'page' : undefined}
               className={classNames(
                 item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
